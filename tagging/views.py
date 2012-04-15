8b39bbd7ec3433f4c9dce4dbca4b5b4019a64879
Filias Heidt <heidt@facepicer.de>
2012-04-15 18:48:07 +0200

    added query view

--- tagging/views.py
+++ tagging/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+import json
+
+from django.shortcuts import render_to_response
+
+from tagging.models import Tag
+
+def query(request):
+    if request.GET['q']:
+        tags = Tag.objects.filter(slug__startswith=q)
+        return render_to_response(json.dumps(tags), mimetype='application/json')
+    return render_to_response(json.dumps('no query'), mimetype='application/json')
