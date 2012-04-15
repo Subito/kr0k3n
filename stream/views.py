dc01f82681c0b47666b07367e50cf4a11628e700
Filias Heidt <subito@whydah.kr0k3n.net>
2012-04-15 18:26:09 +0200

    added details view for items

--- stream/views.py
+++ stream/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 from stream.models import StreamItem
@@ -7,4 +7,11 @@ def home(request, template_name='stream/stream.html'):
     items = StreamItem.objects.order_by('-created')[:20]
     return render_to_response(template_name, 
                               locals(), 
+                              context_instance=RequestContext(request))
+
+def details(request, item_id=None):
+    item = get_object_or_404(StreamItem, pk=item_id)
+    template_name = 'stream/item/%s.html' % item.content_type.name
+    return render_to_response(template_name, 
+                              locals(),
                               context_instance=RequestContext(request))
