7b9adff178350cb307fafa26e3868418cd8c0efd
Filias Heidt <subito.magrathea.kr0k3n.net>
2012-04-18 19:26:30 +0200

    typo in urls

--- tagging/urls.py
+++ tagging/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('tagging.views',
                        url(r'query/', 'query', name='tv_query'),
-                       url(r'add/', 'add', name='tv_add'),
+                       url(r'add/', 'add_tag', name='tv_add'),
 )
