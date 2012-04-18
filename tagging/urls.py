0ddf04d92ec6de17defbf2e50e4d4d034e8aa67c
Filias Heidt <subito.magrathea.kr0k3n.net>
2012-04-18 20:44:34 +0200

    added get_tags url

--- tagging/urls.py
+++ tagging/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('tagging.views',
                        url(r'query/', 'query', name='tv_query'),
                        url(r'add/', 'add_tag', name='tv_add'),
                        url(r'delete/', 'delete_tag', name='tv_delete'),
+                       url(r'get/', 'get_tags', name='tv_get_tags'),
 )
