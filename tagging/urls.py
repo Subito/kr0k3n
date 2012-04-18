630d9a81b90db618953d0473904a6405f5fb4f89
Filias Heidt <subito.magrathea.kr0k3n.net>
2012-04-18 19:24:50 +0200

    added url for adding tags

--- tagging/urls.py
+++ tagging/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('tagging.views',
                        url(r'query/', 'query', name='tv_query'),
+                       url(r'add/', 'add', name='tv_add'),
 )
