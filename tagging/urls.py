cb625185c8d68530aeedf742f15dd2681ef37c0f
Filias Heidt <subito.magrathea.kr0k3n.net>
2012-04-18 20:11:19 +0200

    added delete url for tags

--- tagging/urls.py
+++ tagging/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('tagging.views',
                        url(r'query/', 'query', name='tv_query'),
                        url(r'add/', 'add_tag', name='tv_add'),
+                       url(r'delete/', 'delete_tag', name='tv_delete'),
 )
