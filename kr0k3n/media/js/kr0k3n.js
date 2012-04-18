784d907080f2ec121ff0c491eb1618c151a323d5
Filias Heidt <subito.magrathea.kr0k3n.net>
2012-04-18 20:13:35 +0200

    typo

--- kr0k3n/media/js/kr0k3n.js
+++ kr0k3n/media/js/kr0k3n.js
@@ -20,7 +20,7 @@ $(function(){
                        'id': item.id,
                        'slug': item.name,
                        'item': item_id
-                   }),
+                   });
             },
         onDelete: function(item) {
             item_id = this[0].id.split('-')[2]
@@ -30,6 +30,6 @@ $(function(){
                        'slug': item.name,
                        'item': item_id
                    });
-            },
+            }
     });
 });
