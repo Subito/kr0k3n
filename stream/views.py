1e24c7d5c9e27edbedcb458c0aceeca4ca425a7f
Filias Heidt <subito.magrathea.kr0k3n.net>
2012-04-19 08:50:22 +0200

    if edited, display the form with errors

--- stream/views.py
+++ stream/views.py
@@ -57,8 +57,9 @@ def edit_item(request, item_type=None, item_id=None, template_name='stream/add.h
             successor_item.save()
             messages.success(request, 'Item edited')
             return HttpResponseRedirect(reverse('sv_home'))
-    object = item.content_object
-    form = formset(instance=object)
+    else:
+        object = item.content_object
+        form = formset(instance=object)
     return render_to_response(template_name,
                               locals(),
                               context_instance=RequestContext(request))
