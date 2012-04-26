c1467a7683e574771c8d64311773052a0fba064a
Filias Heidt <subito.magrathea.kr0k3n.net>
2012-04-26 20:46:24 +0200

    made a whole new concept for handling the various items. Makes use of model inheritance and generic relations.

--- stream/models.py
+++ stream/models.py
@@ -1,31 +1,84 @@
 from django.db import models
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from kr0k3n.utils.decorators import receiver_subclasses
 
-from picture.models import Image
-from bookmark.models import Bookmark
-from note.models import Note
+'''
+Base models I use to handle the connections between Objects (Items).
+If I later plan to add another feature, which requires an own model, I
+can simply inherit from "StreamItem" and I get full comment/tagging
+functionality.
+'''
 
-STREAM_MODELS = (Image,
-                 Bookmark,
-                 Note,
-                 )
+class Comment(models.Model):
+    comment = models.TextField()
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type',
+                                               'object_id')
+
+class Tag(models.Model):
+    slug = models.SlugField()
+    created = models.DateTimeField(auto_now_add=True)
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type',
+                                               'object_id')
 
 class StreamItem(models.Model):
-    object_id = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType)
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
-    created = models.DateTimeField(auto_now_add=True)
-    parent = models.ForeignKey('self', blank=True, null=True)
-    display = models.BooleanField(default=True)
-
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type',
+                                               'object_id')
+    
     def __unicode__(self):
-        return '<Item %s>' % self.pk
+        return '<StreamItem %s>' % self.pk
 
-@receiver(post_save)
-def created_stream_item(sender, instance, created, **kwargs):
-    if created and sender in STREAM_MODELS:
+@receiver_subclasses(post_save, Item, 'item_post_save')
+def create_stream_item(sender, instance, created, **kwargs):
+    if created:
         new_item = StreamItem(content_object=instance)
         new_item.save()
+
+class Item(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    display = models.BooleanField(default=True)
+    children = models.ManyToManyField('self', blank=True, null=True)
+    comment = generic.GenericRelation(Comment)
+    tags = generic.GenericRelation(TaggedItem)
+
+    class Meta:
+        abstract = True
+
+'''
+The following models implement the various forms of data I like to
+store
+'''
+
+class Note(Item):
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.title:
+            self.title = self.content[:25] + ' [...]'
+        super(Note, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return '<Note %s>' % self.title
+
+class Bookmark(Item):
+    url = models.URLField()
+
+    def __unicode__(self):
+        return '<Bookmark %s>' % self.url
+
+class Image(Item):
+    image = models.ImageField(upload_to='images/%Y/%m/%d')
+
+    def __unicode__(self):
+        return '<Image %s>' % self.title
